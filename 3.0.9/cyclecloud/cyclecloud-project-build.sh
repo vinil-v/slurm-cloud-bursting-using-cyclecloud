9e601038786143517d36d4743ace7598981fe670
Vinil Vadakkepurakkal <82436391+vinil-v@users.noreply.github.com>
2024-10-02 09:17:39 +0530

    Update - 02
    Updating the setup with custom project and automating the setup

--- 3.0.9/cyclecloud/cyclecloud-project-build.sh
+++ 3.0.9/cyclecloud/cyclecloud-project-build.sh
@@ -5,7 +5,14 @@
 # Date : 01/10/2024
 git clone https://github.com/vinil-v/slurm-cloud-bursting-using-cyclecloud.git
 cd slurm-cloud-bursting-using-cyclecloud
-cyclecloud import_cluster hpc2 -c Slurm-burst-headless -f templates/slurm-309-headless.txt
+
+read -p "Enter Cluster Name: " cluster_name
+echo "Cluster Name: $cluster_name"
+echo "Use the same cluster name: $cluster_name in building the scheduler"
+
+echo "Importing Cluster"
+cyclecloud import_cluster $cluster_name -c Slurm-burst-headless -f templates/slurm-309-headless.txt
+
 # creating custom project and upload it to the locker
 
 CCLOCKERNAME=$(cyclecloud locker list | sed 's/(.*)//')
@@ -15,4 +22,3 @@ SLURM_PROJ_VERSION="3.0.9"
 cyclecloud project fetch https://github.com/Azure/cyclecloud-slurm/releases/$SLURM_PROJ_VERSION slurm$SLURM_PROJ_VERSION
 cd slurm$SLURM_PROJ_VERSION
 cyclecloud project upload "$CCLOCKERNAME"
-
