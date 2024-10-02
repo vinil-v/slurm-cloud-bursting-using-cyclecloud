de047bdaf61d2066c777d42cb9b43d16867c414a
Vinil Vadakkepurakkal <82436391+vinil-v@users.noreply.github.com>
2024-10-02 09:34:01 +0530

    Update cyclecloud-project-build.sh

--- 3.0.9/cyclecloud/cyclecloud-project-build.sh
+++ 3.0.9/cyclecloud/cyclecloud-project-build.sh
@@ -3,15 +3,13 @@
 # This script will fetch the CycleCloud project and upload it to the locker.
 # Author : Vinil Vadakkepurakkal
 # Date : 01/10/2024
-git clone https://github.com/vinil-v/slurm-cloud-bursting-using-cyclecloud.git
-cd slurm-cloud-bursting-using-cyclecloud
-
+set -e
 read -p "Enter Cluster Name: " cluster_name
 echo "Cluster Name: $cluster_name"
 echo "Use the same cluster name: $cluster_name in building the scheduler"
 
 echo "Importing Cluster"
-cyclecloud import_cluster $cluster_name -c Slurm-burst-headless -f slurm-309-headless.txt
+cyclecloud import_cluster $cluster_name -c Slurm-burst-headless -f cyclecloud/slurm-309-headless.txt
 
 # creating custom project and upload it to the locker
 
@@ -21,4 +19,4 @@ echo "Fetching CycleCloud project"
 SLURM_PROJ_VERSION="3.0.9"
 cyclecloud project fetch https://github.com/Azure/cyclecloud-slurm/releases/$SLURM_PROJ_VERSION slurm$SLURM_PROJ_VERSION
 cd slurm$SLURM_PROJ_VERSION
-cyclecloud project upload "$CCLOCKERNAME"
+cyclecloud project upload \"$CCLOCKERNAME\"
