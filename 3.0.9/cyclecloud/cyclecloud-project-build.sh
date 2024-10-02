e0c6bb7d540c175656216622fee38ce35ca9b2b3
Vinil Vadakkepurakkal <82436391+vinil-v@users.noreply.github.com>
2024-10-02 09:35:19 +0530

    Update cyclecloud-project-build.sh
    template location update

--- 3.0.9/cyclecloud/cyclecloud-project-build.sh
+++ 3.0.9/cyclecloud/cyclecloud-project-build.sh
@@ -9,7 +9,7 @@ echo "Cluster Name: $cluster_name"
 echo "Use the same cluster name: $cluster_name in building the scheduler"
 
 echo "Importing Cluster"
-cyclecloud import_cluster $cluster_name -c Slurm-burst-headless -f cyclecloud/slurm-309-headless.txt
+cyclecloud import_cluster $cluster_name -c Slurm-burst-headless -f slurm-309-headless.txt
 
 # creating custom project and upload it to the locker
 
